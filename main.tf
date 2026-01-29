3dde3cf69a714eb6272a86fa069dec58b45aa4ca
MaatheusMartins <matheusmartins0999@gmail.com>
2026-01-29 11:31:34 -0300

    Configuracoes Ansible

--- main.tf
+++ main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "app_server" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name = "iac"
-  user_data = <<-EOF
-		 #!/bin/bash
-		 cd /home/ubuntu
-		 echo "<h1>Feito com Terraform</h1>" > index.html
-		 nohup busybox httpd -f -p 8080 &
-		 EOF
+#  user_data = <<-EOF
+#  		 #!/bin/bash
+#		 cd /home/ubuntu
+#		 echo "<h1>Feito com Terraform</h1>" > index.html
+#		 nohup busybox httpd -f -p 8080 &
+#		 EOF
   tags = {
     Name = "IaC"
   }
