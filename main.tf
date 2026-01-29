a19f407b92ffa0768a07d78313a5a0eecacf0c24
MaatheusMartins <matheusmartins0999@gmail.com>
2026-01-29 09:47:31 -0300

    IaC

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical (Ubuntu oficial)
+
+  filter {
+    name = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+}
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name = "iac"
+  tags = {
+    Name = "Primeira Instancia IaC"
+  }
+}
